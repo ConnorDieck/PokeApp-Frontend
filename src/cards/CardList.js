ff52cc85fa0edfa45dbf660ab2436b941e2e4f0d
Connor Dieck <connordieck@gmail.com>
2022-04-08 10:10:45 -0400

    styling

--- src/cards/CardList.js
+++ src/cards/CardList.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { useSelector } from "react-redux";
-
-import ItemList from "../util/ItemList";
+import { Link } from "react-router-dom";
+import { Grid, Button, Box, Paper } from "@mui/material";
+import { makeStyles, styled } from "@mui/styles";
 
 /**
  * Component for list of cards. 
@@ -10,9 +11,34 @@ import ItemList from "../util/ItemList";
  * 
  */
 
+const Item = styled(Paper)(({ theme }) => ({
+	backgroundColor : theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+	...theme.typography.body2,
+	padding         : theme.spacing(1),
+	textAlign       : "center",
+	color           : theme.palette.text.secondary
+}));
+
 function CardList() {
+	const species = useSelector(st => st.species);
 	const items = useSelector(st => Object.values(st.cards).map(c => ({ ...c, localurl: `/cards/${c.id}` })));
-	return <ItemList title="Cards" items={items} />;
+	return (
+		<Box sx={{ flexGrow: 1 }}>
+			<h1>Pokemon</h1>
+			<Grid container spacing={2}>
+				{items.map(m => (
+					<Grid item xs={4}>
+						<Item key={m.id} style={{ backgroundColor: "#ffffeb" }}>
+							<Link to={m.localurl}>
+								<img src={species[m.speciesId].sprite} />
+							</Link>
+							<p>{m.name}</p>
+						</Item>
+					</Grid>
+				))}
+			</Grid>
+		</Box>
+	);
 }
 
 export default CardList;
